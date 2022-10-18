9169628f6269404fc1216025c17e0472c2278244
muradhossin <murad15-2547@diu.edu.bd>
2022-10-18 22:12:13 +0600

    contact model

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,32 @@
-import 'package:flutter/material.dart';
+import 'dart:js';
 
-void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Contact app'),
-      ),
-    ),
+import 'package:contact_app/pages/details_page.dart';
+import 'package:contact_app/pages/home_page.dart';
+import 'package:contact_app/provider/contact_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ContactProvider(),
+    child: const MyApp(),
   ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomePage.routeName,
+      routes: {
+        HomePage.routeName: (context) => HomePage(),
+        DetailsPage.routeName: (context) => DetailsPage(),
+      },
+    );
+  }
 }
---
+++ lib/pages/details_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget {
+  const DetailsPage({Key? key}) : super(key: key);
+  static const String routeName='/details';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
